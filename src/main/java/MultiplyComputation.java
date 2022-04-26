ddad37a0fe64b1f0e906b681c89a8206d27be675
Barmanturbo <21035407@student.hhs.nl>
2022-04-26 12:36:56 +0200

    multiplycomputation compute() klaar

--- src/main/java/MultiplyComputation.java
+++ src/main/java/MultiplyComputation.java
@@ -1,3 +1,8 @@
-public class MultiplyComputation {
+public class MultiplyComputation implements IComputation{
+
+    @Override
+    public int compute(int num1, int num2){
+        return (num1 * num2);
+    };
     
 }
