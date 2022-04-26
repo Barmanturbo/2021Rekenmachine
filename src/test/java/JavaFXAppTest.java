5f9d4be1fe6ae9c37adb26cd2123f81445573c4a
tolga <t_arslan7@hotmail.com>
2022-04-26 12:04:22 +0200

    Test plus

--- src/test/java/JavaFXAppTest.java
+++ src/test/java/JavaFXAppTest.java
@@ -2,8 +2,14 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class JavaFXAppTest {
-    @Test
-    void aDummyTest() {
-        assertEquals(0,0);
+
+
+        @Test
+        void plusTest () {
+            int expected = 3;
+            JavaFXApp app= new JavaFXApp();
+            int actual =app.computeAdd(1,2);
+
+            assertEquals(expected,actual);
+        }
     }
-}
